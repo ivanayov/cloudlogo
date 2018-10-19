551e8bcd340b5bb504b0a1acf5e99c1ecd555533
Alex Ellis (VMware) <alexellis2@gmail.com>
2018-10-19 13:26:49 +0100

    Initial
    Signed-off-by: Alex Ellis (VMware) <alexellis2@gmail.com>

--- clouddemo/handler.go
+++ clouddemo/handler.go
@@ -1,32 +1,33 @@
 package function
 
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+)
+
+type HomepageTokens struct {
+	Color bool
+}
+
 // Handle a serverless request
 func Handle(req []byte) string {
 	color := true
-	if color {
-		html := `
-		<body style="display:table;position:absolute;height:100%;background-color:#B0D0D3;width:100%;">
-			<div style="display: table-cell;vertical-align: middle;">
-				<div style="margin-left: auto;margin-right: auto;"> 
-					<img src="https://www.openfaas.com/images/openfaas/openfaas.png" style="display:block;margin:auto">
-					<img src="https://www.openfaas.com/images/openfaas/logo.png" style="display:block;margin:auto">
-					<img src="https://irp-cdn.multiscreensite.com/0e5c5b66/dms3rep/multi/tablet/animated-cloud-right-to-left-350x211-350x211.png" style="z-index:5;height:250px;position:relative;top:-70px;left:-10px;display:block;margin:auto">
-				</div>
-			</div>
-		</body>
-		`
-		return html
+
+	var err error
+	tmpl, err := template.ParseFiles("./template/index.html")
+	if err != nil {
+		return fmt.Sprintf("Internal server error with homepage: %s", err.Error())
 	}
-	html := `
-		<body style="display:table;position:absolute;height:100%;background-color:#276ddd;width:100%;">
-			<div style="display: table-cell;vertical-align: middle;">
-				<div style="margin-left: auto;margin-right: auto;"> 
-					<img src="https://www.openfaas.com/images/openfaas/openfaas.png" style="display:block;margin:auto">
-					<img src="https://www.openfaas.com/images/openfaas/logo.png" style="display:block;margin:auto">
-					<img src="https://irp-cdn.multiscreensite.com/0e5c5b66/dms3rep/multi/tablet/animated-cloud-right-to-left-350x211-350x211.png" style="z-index:5;height:250px;position:relative;top:-70px;left:-10px;display:block;margin:auto">
-				</div>
-			</div>
-		</body>
-		`
-	return html
+	var tpl bytes.Buffer
+
+	err = tmpl.Execute(&tpl, HomepageTokens{
+		Color: color,
+	})
+	if err != nil {
+		return fmt.Sprintf("Internal server error with homepage template: %s", err.Error())
+	}
+
+	return string(tpl.Bytes())
+
 }
